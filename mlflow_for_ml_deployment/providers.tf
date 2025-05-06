a98cb371368b6d2a39e642ff3fa075fd7f8c7937
Manuel Gil <manuelgilsitio@gmail.com>
2025-05-06 07:42:39 -0500

    fixing providers

--- mlflow_for_ml_deployment/providers.tf
+++ mlflow_for_ml_deployment/providers.tf
@@ -1,13 +1,4 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
-terraform {
   required_version = ">= 1.0.0"
   backend "azurerm" {
     resource_group_name  = "gilsamasstudyapitfstate"
